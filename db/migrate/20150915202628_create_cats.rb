fec2a2177b8dcc5b358637e43df03d4da522b744
Kat Maldon <katharinamaldon@gmail.com>
2019-07-18 18:53:01 +0000

    Done.

--- db/migrate/20150915202628_create_cats.rb
+++ db/migrate/20150915202628_create_cats.rb
@@ -1,4 +1,7 @@
 class CreateCats < ActiveRecord::Migration
+  
+  
+  
   def up
     create_table :cats do |t|
       t.string :name
@@ -9,5 +12,15 @@ class CreateCats < ActiveRecord::Migration
   
   def down
     drop_table :cats
+  end
+end
+
+class CreateCats < ActiveRecord::Base
+  def change
+    create_table :cats do |t|
+      t.string :name
+      t.integer :age
+      t.string :breed
+    end
   end
 end
